6dff379542891d27c9f966d9898ab89ebfce7b8a
Jessie Anderson <janderson@bds00693.bds-ad.lc>
2016-09-14 15:32:19 -0400

    seeing if jenkins build works

--- bundles/plugin/src/com/blackducksoftware/integration/eclipseplugin/popupmenu/Activator.java
+++ bundles/plugin/src/com/blackducksoftware/integration/eclipseplugin/popupmenu/Activator.java
@@ -1,5 +1,11 @@
 package com.blackducksoftware.integration.eclipseplugin.popupmenu;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -14,7 +20,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -23,22 +29,30 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 *
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+	 * BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	@Override
+	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		if (!getPreferenceStore().contains("minutesBetweenChecks")) {
 			getPreferenceStore().setValue("minutesBetweenChecks", 5);
 		}
-		// System.out.println(getPreferenceStore().getString("minutesBetweenChecks"));
+		if (!getPreferenceStore().contains("activeProject")) {
+			getPreferenceStore().setValue("activeProject", "NONE");
+		}
 		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 *
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
+	 * BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
+	@Override
+	public void stop(final BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
@@ -53,13 +67,78 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
 	 *
-	 * @param path the path
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
+	public static ImageDescriptor getImageDescriptor(final String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
+
+	/**
+	 * Returns a list of projects currently in the workspace
+	 *
+	 * @return list of projects in workspace
+	 */
+	public static IProject[] getProjects() {
+		final IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		return projects;
+	}
+
+	public static int getNumProjects() {
+		return getProjects().length;
+	}
+
+	public static String[][] getProjectNames() {
+		final IProject[] projects = getProjects();
+		final String[][] names = new String[projects.length + 1][2];
+		for (int i = 0; i < projects.length; i++) {
+			names[i][0] = projects[i].toString().split("/")[1];
+			names[i][1] = projects[i].toString();
+		}
+		names[projects.length][0] = "no active project";
+		names[projects.length][1] = "NONE";
+		return names;
+	}
+
+	public static void printProjectInfo() {
+		final IProject[] projects = getProjects();
+		for (int i = 0; i < projects.length; i++) {
+			try {
+				System.out.print("name: ");
+				System.out.println(projects[i].getDescription().getName());
+				System.out.println();
+				System.out.println("nature ids:");
+				final String[] natureIds = projects[i].getDescription().getNatureIds();
+				for (int j = 0; j < natureIds.length; j++) {
+					System.out.println(natureIds[j]);
+				}
+				System.out.println();
+				if (projects[i].hasNature(JavaCore.NATURE_ID)) {
+					final IJavaProject javaProject = JavaCore.create(projects[i]);
+					final IClasspathEntry[] classpathEntries = javaProject.getRawClasspath();
+					final String[] requiredProjectNames = javaProject.getRequiredProjectNames();
+					System.out.println("entries:");
+					for (int j = 0; j < classpathEntries.length; j++) {
+						final IClasspathEntry entry = classpathEntries[j];
+						System.out.println("kind: " + entry.getEntryKind());
+						System.out.println("path: " + entry.getPath().toOSString());
+					}
+					System.out.println();
+					System.out.println("required projects: ");
+					for (int j = 0; j < requiredProjectNames.length; j++) {
+						System.out.println(requiredProjectNames[j]);
+					}
+				}
+				System.out.println("-------------------------");
+			} catch (final CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
--- bundles/plugin/src/com/blackducksoftware/integration/eclipseplugin/preferences/ScanningPreferences.java
+++ bundles/plugin/src/com/blackducksoftware/integration/eclipseplugin/preferences/ScanningPreferences.java
@@ -2,6 +2,7 @@ package com.blackducksoftware.integration.eclipseplugin.preferences;
 
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
 // import org.eclipse.jface.preference.PreferencePage;
 //import org.eclipse.jface.resource.ImageDescriptor;
 // import org.eclipse.swt.widgets.Composite;
@@ -17,13 +18,18 @@ public class ScanningPreferences extends FieldEditorPreferencePage implements IW
 	public void init(final IWorkbench workbench) {
 		// obtain preferences
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		Activator.printProjectInfo();
 	}
 
 	@Override
 	protected void createFieldEditors() {
 		final IntegerFieldEditor minutesBetweenChecks = new IntegerFieldEditor("minutesBetweenChecks",
 				"Minutes between checks", getFieldEditorParent());
+		final RadioGroupFieldEditor activeProjectSelector = new RadioGroupFieldEditor("activeProject", "Active Project",
+				Activator.getNumProjects(), Activator.getProjectNames(), getFieldEditorParent());
 		addField(minutesBetweenChecks);
+		addField(activeProjectSelector);
+
 	}
 
 }
