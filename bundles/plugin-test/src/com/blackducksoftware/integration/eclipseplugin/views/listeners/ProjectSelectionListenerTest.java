61d1ac107315554c4f53cf8a9a02c25c6eed217b
Jessie Anderson <janderson@bds01041.bds-ad.lc>
2016-11-10 13:34:30 -0500

    project selection listener test finished

--- bundles/plugin-test/src/com/blackducksoftware/integration/eclipseplugin/views/listeners/ProjectSelectionListenerTest.java
+++ bundles/plugin-test/src/com/blackducksoftware/integration/eclipseplugin/views/listeners/ProjectSelectionListenerTest.java
@@ -1,7 +1,12 @@
 package com.blackducksoftware.integration.eclipseplugin.views.listeners;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.ui.IWorkbenchPart;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,27 +24,56 @@ public class ProjectSelectionListenerTest {
 	@Mock
 	IWorkbenchPart part;
 	@Mock
+	ISelection sel;
+	@Mock
 	IStructuredSelection structuredSel;
+	@Mock
+	IAdaptable adaptable;
+	@Mock
+	IProject project;
+	@Mock
+	TableViewer table;
+	@Mock
+	IProjectDescription description;
+	@Mock
+	CoreException e;
+
+	private final String PROJECT_NAME = "name";
 
 	private final String SELECTED_PROJECT_NAME = "name";
 
-	private void setUpRelationships() {
-
+	private void setUpRelationships() throws CoreException {
+		Mockito.when(structuredSel.getFirstElement()).thenReturn(adaptable);
+		Mockito.when(adaptable.getAdapter(IProject.class)).thenReturn(project);
+		Mockito.when(project.getDescription()).thenReturn(description);
+		Mockito.when(description.getName()).thenReturn(PROJECT_NAME);
+		Mockito.when(view.getTable()).thenReturn(table);
 	}
 
 	@Test
 	public void testNotStructuredSelection() {
-		final ISelection sel = Mockito.mock(ISelection.class);
 		final ProjectSelectionListener listener = new ProjectSelectionListener(view);
 		listener.selectionChanged(part, sel);
 		Mockito.verify(view, Mockito.times(0)).setLastSelectedProjectName(SELECTED_PROJECT_NAME);
 	}
 
 	@Test
-	public void testStructuredSelection() {
-		final ISelection sel = Mockito.mock(ISelection.class,
-				Mockito.withSettings().extraInterfaces(IStructuredSelection.class));
+	public void testStructuredSelection() throws CoreException {
+		setUpRelationships();
+		final ProjectSelectionListener listener = new ProjectSelectionListener(view);
+		listener.selectionChanged(part, structuredSel);
+		Mockito.verify(view, Mockito.times(1)).setLastSelectedProjectName(PROJECT_NAME);
+		Mockito.verify(view, Mockito.times(1)).setTableInput(PROJECT_NAME);
+	}
 
+	@Test
+	public void testWhenCoreExceptionThrown() throws CoreException {
+		setUpRelationships();
+		Mockito.when(project.getDescription()).thenThrow(e);
+		final ProjectSelectionListener listener = new ProjectSelectionListener(view);
+		listener.selectionChanged(part, structuredSel);
+		Mockito.verify(view, Mockito.times(1)).setLastSelectedProjectName("");
+		Mockito.verify(view, Mockito.times(1)).setTableInput("");
 	}
 
 }
